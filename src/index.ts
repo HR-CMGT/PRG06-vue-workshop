19730f5aa7fea0f060e4236849c30d9ddcd8607b
Kokodoko <benni_jolink@hotmail.com>
2017-12-11 11:04:47 +0100

    Added app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 console.log("create an instance of app in index.ts")
 
-//import App from "./components/App.vue"
-//new App({ el: "#app" })
+import App from "./components/App.vue"
+new App({ el: "#app" })
