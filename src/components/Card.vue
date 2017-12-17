4c40d1b34daeae456391a694c2c9f3a3d9536864
Erik Katerborg <benni_jolink@hotmail.com>
2017-12-17 14:30:43 +0100

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -40,31 +40,4 @@ export default class Card extends Vue {
     padding:12px; 
     height:170px;
 }
-
-.list {
-    background-color:white;
-    display:flex;
-    margin:2vw;
-    width:80vw; height:200px;
-    overflow: hidden;
-    box-shadow: 0px 0px 15px rgba(0,0,0,0.4);
-    transition: all 0.3s ease-in-out;
-    cursor: pointer;
-}
-.list:hover {
-	transform:scale(1.05);
-	box-shadow: 0px 0px 45px rgba(0,0,0,0.8);
-}
-.list div:nth-child(1){
-    background-color:slategray;
-    background-size:cover;
-    background-position: center;
-    width:200px;
-    height:200px;
-}
-
-.list div:nth-child(2){
-    padding:12px; 
-    height:200px;
-}
 </style>
